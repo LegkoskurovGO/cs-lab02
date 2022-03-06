46ae9dc5b6bcf960cc29b3392cb88c4fe9c426df
Alice (LegkoskurovGO) <alice@a.b>
2022-03-06 19:44:52 +0300

    code: Добавил вывод максимума

--- project/main.cpp
+++ project/main.cpp
@@ -12,5 +12,12 @@ int main() {
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
     
+    if (a > b) {
+        cout << "Maximum is " << a << '\n';
+    }
+    else {
+        cout << "Maximum is " << b << '\n';
+    }
+    
     return 0;
 }
